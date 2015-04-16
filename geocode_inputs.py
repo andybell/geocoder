9cf1558e9c02ef816040268acd8ffec9bd1cc164
Andy Bell <ambell@ucdavis.edu>
2015-04-16 13:40:15 -0700

    sys argv for input file name

--- geocode_inputs.py
+++ geocode_inputs.py
@@ -1,22 +1,30 @@
+import sys
 import os
 import geocoder
 
-# file name
-file_2B_geocoded = 'dairies_2_geo.txt' # TODO make this a cmd line input?
+print len(sys.argv)
 
-#get folder location
+# get folder location
 wd = os.path.dirname(os.path.abspath(__file__))  # gets the path of *this file* and then gets the dir it's in
 print(wd)
 
-# txt file with address data
-# address should be one line per address with no header
-address = os.path.join(wd, file_2B_geocoded)
+if len(sys.argv) == 2: # checks if a single argument was given
 
-# base name of file
-base, extension = os.path.splitext(address)
+	# file name from system argument
+	file_2B_geocoded = sys.argv[1]  # get's file name from argument provided from user
 
-# output text file
-output = os.path.join(wd, base + "_geocoder_results" + extension) # adds geocoded_results to input file name
+	# txt file with address data
+	# address should be one line per address with no header
+	address = os.path.join(wd, file_2B_geocoded)
 
-print output
-geocoder.parse_file(address, output)
+	# base name of file
+	base, extension = os.path.splitext(address)
+
+	# output text file
+	output = os.path.join(wd, base + "_geocoder_results" + extension) # adds geocoded_results to input file name
+
+	print output
+	geocoder.parse_file(address, output)
+
+else:
+	print("ERROR: please use text file name as a cmd line argument")
