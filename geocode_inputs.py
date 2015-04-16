f8ed5905a0ee609adb49271a859e340daefaa458
Andy Bell <ambell@ucdavis.edu>
2015-04-16 13:18:45 -0700

    geocoder automatically make export txt file

--- geocode_inputs.py
+++ geocode_inputs.py
@@ -1,14 +1,22 @@
 import os
 import geocoder
 
+# file name
+file_2B_geocoded = 'dairies_2_geo.txt' # TODO make this a cmd line input?
+
 #get folder location
 wd = os.path.dirname(os.path.abspath(__file__))  # gets the path of *this file* and then gets the dir it's in
 print(wd)
 
-# address path one line per address
-address = os.path.join(wd, 'address_list.txt')
+# txt file with address data
+# address should be one line per address with no header
+address = os.path.join(wd, file_2B_geocoded)
 
-# output shapefile
-output = os.path.join(wd, 'output_geocoder.txt')
+# base name of file
+base, extension = os.path.splitext(address)
 
+# output text file
+output = os.path.join(wd, base + "_geocoder_results" + extension) # adds geocoded_results to input file name
+
+print output
 geocoder.parse_file(address, output)
